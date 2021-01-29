5b871e785d816b28e68c78595764449979d84231
Alex T <a.tabachuk@geniusee.com>
2021-01-29 02:05:22 +0200

    total amount of digitalbits

--- src/ledger/LedgerManagerImpl.cpp
+++ src/ledger/LedgerManagerImpl.cpp
@@ -76,7 +76,7 @@ const uint32_t LedgerManager::GENESIS_LEDGER_VERSION = 0;
 const uint32_t LedgerManager::GENESIS_LEDGER_BASE_FEE = 100;
 const uint32_t LedgerManager::GENESIS_LEDGER_BASE_RESERVE = 100000000;
 const uint32_t LedgerManager::GENESIS_LEDGER_MAX_TX_SIZE = 100;
-const int64_t LedgerManager::GENESIS_LEDGER_TOTAL_COINS = 1000000000000000000;
+const int64_t LedgerManager::GENESIS_LEDGER_TOTAL_COINS = 200000000000000000;
 
 std::unique_ptr<LedgerManager>
 LedgerManager::create(Application& app)
